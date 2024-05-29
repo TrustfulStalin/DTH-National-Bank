4e38176d0bc7b160cc7e26f25e48edbe33069a05
Tevon Talbert <TevonTalbert@hotmail.com>
2024-05-29 18:57:48 -0500

    stuff again

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import Header from './Components/Header';
+
 import './App.css';
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
-import Tron from './Components/Form';
+
 import Home from './pages/home';
 import MyForm from './pages/Create';
-import Transactions from './pages/Trans';
+
 import Login from './pages/LoginPage';
 import { useState, useEffect } from "react";
 import Real from './pages/Trans2';
 import Per from './pages/PerUser';
-import ApiPage from './pages/trans3';
+
 
 function App() {
   const [user, setUser] = useState(null);
